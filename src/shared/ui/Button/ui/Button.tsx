bcdc112ed35dcd104b6b505b21546c88f66ff11d
Kirill Palkin <k.palkin@rootcode.ru>
2023-09-04 11:46:14 +0500

    lesson 13

---
+++ src/shared/ui/Button/ui/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Button.module.scss';
+
+export enum ThemeButton {
+   CLEAR = 'clear',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  additionalClasses?: string[],
+  theme?: ThemeButton,
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { additionalClasses = [], theme = ThemeButton.CLEAR, children, ...otherProps } = props;
+
+  return (
+    <button 
+      type="button" 
+      className={classNames(cls.Button, {}, [...additionalClasses, cls[theme]])} 
+      {...otherProps}
+    >
+      { children }
+    </button>
+  )
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 interface NavbarProps {
   additionalClasses?: string[],
@@ -9,11 +10,10 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = (props) => {
   const { additionalClasses = [] } = props;
-  console.log('additionalClasses: ', additionalClasses);
-  console.log('classNames(cls.Navbar, {}, [...additionalClasses]): ', classNames(cls.Navbar, {}, [...additionalClasses]));
   
   return (
     <div className={classNames(cls.Navbar, {}, [...additionalClasses])}>
+      <ThemeSwitcher />
       <AppLink to={'/'} theme={AppLinkTheme.PRIMARY}>Главная</AppLink>
       <AppLink to={'/about'} theme={AppLinkTheme.SECONDARY}>О сайте</AppLink>
     </div>
