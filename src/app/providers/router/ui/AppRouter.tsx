170f0e427b6428e00e8370f170b7bc1babce3724
Kirill Palkin <k.palkin@rootcode.ru>
2023-09-06 10:44:54 +0500

    lessong 22

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,3 +1,4 @@
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { FC, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
@@ -6,11 +7,13 @@ import { PageLoader } from 'widgets/PageLoader';
 export const AppRouter: FC = () => (
    <Suspense fallback={<PageLoader />}>
       <div className="page-wrapper">
-         <Routes>
-            {Object.values(routeConfig).map(({ path, element }) => (
-               <Route key={path} path={path} element={element} />
-            ))}
-         </Routes>
+         <ErrorBoundary>
+            <Routes>
+               {Object.values(routeConfig).map(({ path, element }) => (
+                  <Route key={path} path={path} element={element} />
+               ))}
+            </Routes>
+         </ErrorBoundary>
       </div>
    </Suspense>
 );
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  additionalClasses?: string[],
+}
+
+export const PageError: FC<PageErrorProps> = (props) => {
+   const { additionalClasses = [] } = props;
+
+   const { t } = useTranslation();
+
+   const reloadPage = () => {
+      window.location.reload();
+   };
+
+   return (
+      <div className={classNames(cls.PageError, {}, [...additionalClasses])}>
+         <p>{t('Произошла непредвиденная ошибка')}</p>
+         <Button onClick={reloadPage}>
+            {t('Обновить страницу')}
+         </Button>
+      </div>
+   );
+};
