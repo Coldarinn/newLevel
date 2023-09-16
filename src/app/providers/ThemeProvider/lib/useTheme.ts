a18b5817a3ead74e69479da7d537d1ea7f2a29c7
Kirill Palkin <k.palkin@rootcode.ru>
2023-09-16 14:24:50 +0500

    lesson 44 - third theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -12,7 +12,21 @@ export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme;
+    switch (theme) {
+    case Theme.DARK:
+      newTheme = Theme.LIGHT;
+      break;
+    case Theme.LIGHT:
+      newTheme = Theme.BLUE;
+      break;
+    case Theme.BLUE:
+      newTheme = Theme.DARK;
+      break;
+    default:
+      newTheme = Theme.DARK;
+      break;
+    }
     setTheme?.(newTheme);
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
