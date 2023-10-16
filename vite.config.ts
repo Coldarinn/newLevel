fbdc8a81bd49cbd8b472c3ba4a26e5c57b63c44e
Kirill Palkin <k.palkin@rootcode.ru>
2023-10-16 21:22:40 +0500

    lesson 85 - vite

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [
+    // svgr({exportAsDefault}),
+    react(),
+    svgr({ include: '**/*.svg' }),
+  ],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+    ],
+  },
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __API_URL__: JSON.stringify('http://localhost:8000'),
+    __PROJECT__: JSON.stringify('frontend'),
+  },
+});
